d94c3eae18f47e4731300a4cfe482983da88629d
tomioka-k <tomioka-k@tajima.co.jp>
2021-10-09 23:58:37 +0900

    blog-page edit, create getStaticProps

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,7 +1,22 @@
 import Layout from "../components/Layout";
+import { getAllPostData } from "../lib/posts";
+import Post from "../components/post";
 
-const Blog = () => {
-  return <Layout title="Blog">Blog page</Layout>;
+const Blog = ({ posts }) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostData();
+  return {
+    props: { posts },
+  };
+}
